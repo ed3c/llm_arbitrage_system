5891311d521ca857d1fc7bfb754e0d3b5d169753
ed3c <mcnum01@gmail.com>
2026-08-14 19:31:43 +0800

    feat(phase8): export separation-of-duties APIs

--- src/llm_arbitrage_system/experiments/__init__.py
+++ src/llm_arbitrage_system/experiments/__init__.py
@@ -12,6 +12,15 @@ from llm_arbitrage_system.experiments.campaign_runner import (
 )
 from llm_arbitrage_system.experiments.campaign_store import CampaignStore
 from llm_arbitrage_system.experiments.dataset import DatasetSnapshot, load_jsonl_dataset
+from llm_arbitrage_system.experiments.decision_request import (
+    DecisionRequestSnapshot,
+    load_decision_request,
+)
+from llm_arbitrage_system.experiments.decision_request_signing import (
+    DecisionRequestAttestationResult,
+    sign_decision_request,
+    verify_decision_request_attestation,
+)
 from llm_arbitrage_system.experiments.evaluation import (
     PlannedEvaluationRunResult,
     run_planned_evaluation,
@@ -22,6 +31,26 @@ from llm_arbitrage_system.experiments.oos_statistics import (
     build_oos_statistics,
 )
 from llm_arbitrage_system.experiments.registry import ExperimentRegistry
+from llm_arbitrage_system.experiments.review_evidence import (
+    ReviewEvidenceAttestationResult,
+    ReviewRecordSnapshot,
+    load_review_record,
+    sign_review_record,
+    verify_review_record_attestation,
+)
+from llm_arbitrage_system.experiments.review_quorum import (
+    ReviewQuorumEnvelope,
+    ReviewQuorumEnvelopeSnapshot,
+    ReviewQuorumInputSnapshot,
+    build_review_quorum,
+    load_review_quorum_envelope,
+    load_review_quorum_inputs,
+)
+from llm_arbitrage_system.experiments.review_quorum_signing import (
+    ReviewQuorumAttestationResult,
+    sign_review_quorum_envelope,
+    verify_review_quorum_attestation,
+)
 from llm_arbitrage_system.experiments.runner import ExperimentRunResult, run_experiment
 from llm_arbitrage_system.experiments.selection_diagnostics import (
     SelectionDiagnosticsReport,
@@ -70,11 +99,19 @@ __all__ = [
     "CampaignSpecSnapshot",
     "CampaignStore",
     "DatasetSnapshot",
+    "DecisionRequestAttestationResult",
+    "DecisionRequestSnapshot",
     "EvaluationValuationInput",
     "ExperimentRegistry",
     "ExperimentRunResult",
     "OOSStatisticsReport",
     "PlannedEvaluationRunResult",
+    "ReviewEvidenceAttestationResult",
+    "ReviewQuorumAttestationResult",
+    "ReviewQuorumEnvelope",
+    "ReviewQuorumEnvelopeSnapshot",
+    "ReviewQuorumInputSnapshot",
+    "ReviewRecordSnapshot",
     "SelectionDiagnosticsReport",
     "SelectionDiagnosticsSnapshot",
     "SelectionDossier",
@@ -86,11 +123,16 @@ __all__ = [
     "StatisticsReportSnapshot",
     "TerminalMarksSnapshot",
     "build_oos_statistics",
+    "build_review_quorum",
     "build_selection_diagnostics",
     "build_selection_dossier",
     "campaign_status",
     "load_campaign_spec",
+    "load_decision_request",
     "load_jsonl_dataset",
+    "load_review_quorum_envelope",
+    "load_review_quorum_inputs",
+    "load_review_record",
     "load_selection_diagnostics",
     "load_selection_dossier",
     "load_selection_policy",
@@ -101,10 +143,16 @@ __all__ = [
     "run_experiment",
     "run_planned_evaluation",
     "sign_bundle",
+    "sign_decision_request",
+    "sign_review_quorum_envelope",
+    "sign_review_record",
     "sign_selection_dossier",
     "sign_statistics_report",
     "value_bundle",
     "verify_attestation",
+    "verify_decision_request_attestation",
+    "verify_review_quorum_attestation",
+    "verify_review_record_attestation",
     "verify_selection_dossier_attestation",
     "verify_statistics_attestation",
 ]
